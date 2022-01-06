627608d25bbda999efabb7a4d130b457f07a908f
Takeshi Yanagihara <momotarousu@gmail.com>
2022-01-06 22:46:53 +0900

    :tada: init

---
+++ pages/[slug].tsx
@@ -0,0 +1,25 @@
+import { NextPage } from "next"
+import { useRouter } from "next/router"
+import { useEffect, useState } from "react"
+
+const ShowPage: NextPage = () => {
+  const router = useRouter()
+  const slug = router.query.slug as string
+  const [data, setData] = useState<any>({})
+  const [loading, setLoading] = useState<boolean>(false)
+
+  useEffect(() => {
+    setLoading(true)
+    // TODO: ここをCloud FunctionsのエンドポイントのURLに変更する
+    fetch(`https://example.com/${slug}`)
+      .then((res) => res.json())
+      .then(setData)
+      .finally(() => setLoading(false))
+  }, [slug])
+
+  if (loading) return <span>Loading...</span>
+
+  return <div>{JSON.stringify(data)}</div>
+}
+
+export default ShowPage
